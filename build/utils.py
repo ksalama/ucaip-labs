0eab1a5e13efba20327fe3b1139d0e726f0c342f
ksalama <khalidsalama@google.com>
2021-05-24 22:19:24 +0000

    Create build utils

--- build/utils.py
+++ build/utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Create endpoint and deploy a model to Vertex AI."""
+"""Utilities for deploying pipelines and models to Vertex AI."""
 
 
 import argparse
@@ -26,7 +26,9 @@ SCRIPT_DIR = os.path.dirname(
 )
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, "..")))
 
+from src.pipelines import runner
 from src.utils.ucaip_utils import AIPUtils
+from google.cloud import storage
 
 SERVING_SPEC_FILEPATH = 'build/serving_resources_spec.json'
 
@@ -57,6 +59,16 @@ def get_args():
         '--model-display-name', 
         type=str,
     )
+    
+    parser.add_argument(
+        '--pipeline-name', 
+        type=str,
+    )
+    
+    parser.add_argument(
+        '--pipelines-store', 
+        type=str,
+    )
 
     return parser.parse_args()
 
@@ -77,20 +89,54 @@ def deploy_model(project, region, endpoint_display_name, model_display_name, ded
         dedicated_serving_resources_spec).result()
     logging.info(f"Model {model_display_name} is deployed.")
     return result
+
+def compile_pipeline(pipeline_name):
+    pipeline_definition_file = f"{pipeline_name}.json"
+    pipeline_definition = runner.compile_pipeline(pipeline_definition_file)
+    return pipeline_definition
+
+def upload_pipeline(pipeline_name, pipelines_store):
+    pipeline_definition_file = f"{pipeline_name}.json"
+    storage_client = storage.Client()
+    gcs_location_parts = pipelines_store.replace("gs://", "").split("/")
+    bucket_name = gcs_location_parts[0]
+    destination_blob_name = "/".join(gcs_location_parts[1:])
+    
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(pipeline_definition_file)
+    return os.path.join(gcs_location, pipeline_definition_file)
+
     
 
 def main():
     args = get_args()
     
-    if args.mode == 'endpoint':
+    if args.mode == 'create-endpoint':
+        
+        if not args.project:
+            raise ValueError("project must be supplied.")
+        if not args.region:
+            raise ValueError("region must be supplied.")
+        if not args.endpoint_display_name:
+            raise ValueError("endpoint_display_name must be supplied.")
+            
         result = create_endpoint(
             args.project, 
             args.region, 
             args.endpoint_display_name
         )
-    else:
+    elif args.mode == 'deploy-model':
+        
+        if not args.project:
+            raise ValueError("project must be supplied.")
+        if not args.region:
+            raise ValueError("region must be supplied.")
+        if not args.endpoint_display_name:
+            raise ValueError("endpoint-display-name must be supplied.")
         if not args.model_display_name:
             raise ValueError("model-display-name must be supplied.")
+            
         with open(SERVING_SPEC_FILEPATH) as json_file:
             dedicated_serving_resources_spec = json.load(json_file)
         logging.info(f"serving resources: {dedicated_serving_resources_spec}")
@@ -101,6 +147,19 @@ def main():
             args.model_display_name,
             dedicated_serving_resources_spec
         )
+    elif args.mode == 'compile-pipeline':
+        if not args.pipeline_name:
+            raise ValueError("pipeline-name must be supplied.")
+        result = compile_pipeline(args.pipeline_name):
+    elif args.mode == 'upload-pipeline':
+        if not args.pipeline_name:
+            raise ValueError("pipeline-name must be supplied.")
+        if not args.pipelines_store:
+            raise ValueError("pipelines-store must be supplied.")
+        result = upload_pipeline(pipeline_name, pipelines_store)
+    else:
+        raise ValueError(f"Invalid mode {args.mode}.")
+        
     logging.info(result)
         
     
