15b1f77d331720a2103499f18c9561863a75426a
ksalama <khalidsalama@google.com>
2021-05-24 17:00:15 +0000

    Updaye docstring

--- build/deploy-model.py
+++ build/deploy-model.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Create endpoint and deploy a model to AI Platform."""
+"""Create endpoint and deploy a model to Vertex AI."""
 
 
 import argparse
