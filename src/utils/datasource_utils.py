00a0e15ad284d65686f411083d4f3b69bc1d8f88
ksalama <khalidsalama@google.com>
2021-05-24 22:19:50 +0000

    Format source query

--- src/utils/datasource_utils.py
+++ src/utils/datasource_utils.py
@@ -25,24 +25,24 @@ def get_training_source_query(
     _, bq_dataset_name, bq_table_name = bq_source_uri.replace("g://", "").split('.')
     
     query = f'''
-        SELECT 
-            IF(trip_month IS NULL, -1, trip_month) trip_month,	
-            IF(trip_day IS NULL, -1, trip_day) trip_day,
-            IF(trip_day_of_week IS NULL, -1, trip_day_of_week) trip_day_of_week,
-            IF(trip_hour IS NULL, -1, trip_hour) trip_hour,	
-            IF(trip_seconds IS NULL, -1, trip_seconds) trip_seconds,
-            IF(trip_miles IS NULL, -1, trip_miles) trip_miles,
-            IF(payment_type IS NULL, 'NA', payment_type) payment_type,
-            IF(pickup_grid IS NULL, 'NA', pickup_grid) pickup_grid,
-            IF(dropoff_grid IS NULL, 'NA', dropoff_grid) dropoff_grid,
-            IF(euclidean IS NULL, -1, euclidean) euclidean,
-            IF(loc_cross IS NULL, 'NA', loc_cross) loc_cross,
-            tip_bin
-        FROM {bq_dataset_name}.{bq_table_name} 
-        WHERE data_split = '{data_split}'
+    SELECT 
+        IF(trip_month IS NULL, -1, trip_month) trip_month,	
+        IF(trip_day IS NULL, -1, trip_day) trip_day,
+        IF(trip_day_of_week IS NULL, -1, trip_day_of_week) trip_day_of_week,
+        IF(trip_hour IS NULL, -1, trip_hour) trip_hour,	
+        IF(trip_seconds IS NULL, -1, trip_seconds) trip_seconds,
+        IF(trip_miles IS NULL, -1, trip_miles) trip_miles,
+        IF(payment_type IS NULL, 'NA', payment_type) payment_type,
+        IF(pickup_grid IS NULL, 'NA', pickup_grid) pickup_grid,
+        IF(dropoff_grid IS NULL, 'NA', dropoff_grid) dropoff_grid,
+        IF(euclidean IS NULL, -1, euclidean) euclidean,
+        IF(loc_cross IS NULL, 'NA', loc_cross) loc_cross,
+        tip_bin
+    FROM {bq_dataset_name}.{bq_table_name} 
+    WHERE data_split = '{data_split}'
     '''
     if limit:
-        query += f'\n limit {limit}'
+        query += f'LIMIT {limit}'
     return query
 
 
