55bc30c1b374a43d2ea488a04096aa359d76a055
ksalama <khalidsalama@google.com>
2021-05-26 22:56:24 +0000

    Add trainer.evaluate

--- src/model_training/trainer.py
+++ src/model_training/trainer.py
@@ -80,3 +80,27 @@ def train(
     logging.info("Model training completed.")
 
     return classifier
+
+
+def evaluate(
+    model,
+    data_dir,
+    raw_schema_location,
+    tft_output_dir,
+    hyperparams
+):
+    logging.info(f"Loading raw schema from {raw_schema_location}")
+    raw_schema = tfdv.load_schema_text(raw_schema_location)
+    raw_feature_spec = schema_utils.schema_as_feature_spec(raw_schema).feature_spec
+
+    logging.info(f"Loading tft output from {tft_output_dir}")
+    tft_output = tft.TFTransformOutput(tft_output_dir)
+    transformed_feature_spec = tft_output.transformed_feature_spec()
+    
+    eval_dataset = data.get_dataset(
+        data_dir,
+        transformed_feature_spec,
+        hyperparams["batch_size"],
+    )
+    
+    return model.evaluate(eval_dataset)
