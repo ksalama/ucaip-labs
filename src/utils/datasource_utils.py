6b2931f0f53e19d7c1fad7f3a3a3e788a24c18e6
ksalama <khalidsalama@google.com>
2021-05-30 20:30:16 +0000

    Add datasource_utils tests

--- src/utils/datasource_utils.py
+++ src/utils/datasource_utils.py
@@ -15,6 +15,35 @@
 
 from src.utils.vertex_utils import VertexClient
 
+def _get_source_query(bq_dataset_name, bq_table_name, data_split, limit=None):
+    query = f"""
+    SELECT 
+        IF(trip_month IS NULL, -1, trip_month) trip_month,
+        IF(trip_day IS NULL, -1, trip_day) trip_day,
+        IF(trip_day_of_week IS NULL, -1, trip_day_of_week) trip_day_of_week,
+        IF(trip_hour IS NULL, -1, trip_hour) trip_hour,
+        IF(trip_seconds IS NULL, -1, trip_seconds) trip_seconds,
+        IF(trip_miles IS NULL, -1, trip_miles) trip_miles,
+        IF(payment_type IS NULL, 'NA', payment_type) payment_type,
+        IF(pickup_grid IS NULL, 'NA', pickup_grid) pickup_grid,
+        IF(dropoff_grid IS NULL, 'NA', dropoff_grid) dropoff_grid,
+        IF(euclidean IS NULL, -1, euclidean) euclidean,
+        IF(loc_cross IS NULL, 'NA', loc_cross) loc_cross"""
+    if data_split:
+        query += f""",
+        tip_bin
+    FROM {bq_dataset_name}.{bq_table_name} 
+    WHERE data_split = '{data_split}'
+    """
+    else:
+        query += f"""
+    FROM {bq_dataset_name}.{bq_table_name} 
+    """
+    if limit:
+        query += f"LIMIT {limit}"
+
+    return query
+    
 
 def get_training_source_query(
     project, region, dataset_display_name, data_split, limit=None
@@ -28,46 +57,9 @@ def get_training_source_query(
     ]
     _, bq_dataset_name, bq_table_name = bq_source_uri.replace("g://", "").split(".")
 
-    query = f"""
-    SELECT 
-        IF(trip_month IS NULL, -1, trip_month) trip_month,	
-        IF(trip_day IS NULL, -1, trip_day) trip_day,
-        IF(trip_day_of_week IS NULL, -1, trip_day_of_week) trip_day_of_week,
-        IF(trip_hour IS NULL, -1, trip_hour) trip_hour,	
-        IF(trip_seconds IS NULL, -1, trip_seconds) trip_seconds,
-        IF(trip_miles IS NULL, -1, trip_miles) trip_miles,
-        IF(payment_type IS NULL, 'NA', payment_type) payment_type,
-        IF(pickup_grid IS NULL, 'NA', pickup_grid) pickup_grid,
-        IF(dropoff_grid IS NULL, 'NA', dropoff_grid) dropoff_grid,
-        IF(euclidean IS NULL, -1, euclidean) euclidean,
-        IF(loc_cross IS NULL, 'NA', loc_cross) loc_cross,
-        tip_bin
-    FROM {bq_dataset_name}.{bq_table_name} 
-    WHERE data_split = '{data_split}'
-    """
-    if limit:
-        query += f"LIMIT {limit}"
-    return query
+    return _get_training_source_query(bq_dataset_name, bq_table_name, data_split, limit)
 
 
 def get_serving_source_query(bq_dataset_name, bq_table_name, limit=None):
 
-    query = f"""
-        SELECT 
-            IF(trip_month IS NULL, -1, trip_month) trip_month,	
-            IF(trip_day IS NULL, -1, trip_day) trip_day,
-            IF(trip_day_of_week IS NULL, -1, trip_day_of_week) trip_day_of_week,
-            IF(trip_hour IS NULL, -1, trip_hour) trip_hour,	
-            IF(trip_seconds IS NULL, -1, trip_seconds) trip_seconds,
-            IF(trip_miles IS NULL, -1, trip_miles) trip_miles,
-            IF(payment_type IS NULL, 'NA', payment_type) payment_type,
-            IF(pickup_grid IS NULL, 'NA', pickup_grid) pickup_grid,
-            IF(dropoff_grid IS NULL, 'NA', dropoff_grid) dropoff_grid,
-            IF(euclidean IS NULL, -1, euclidean) euclidean,
-            IF(loc_cross IS NULL, 'NA', loc_cross) loc_cross
-        FROM {bq_dataset_name}.{bq_table_name} 
-        WHERE data_split = 'TEST'
-    """
-    if limit:
-        query += f"\n limit {limit}"
-    return query
+    return _get_source_query(bq_dataset_name, bq_table_name, data_split=None, limit=None)
